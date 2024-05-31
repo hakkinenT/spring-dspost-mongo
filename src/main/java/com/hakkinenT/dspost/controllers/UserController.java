a9e566473079c8f44b7ffeb6b834a07983d6f62f
Tawanna <thol_16@hotmail.com>
2024-05-31 12:01:48 -0300

    GET users by id

--- src/main/java/com/hakkinenT/dspost/controllers/UserController.java
+++ src/main/java/com/hakkinenT/dspost/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.hakkinenT.dspost.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,12 @@ public class UserController {
         List<UserDTO> users = userService.findAll();
 
         return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        UserDTO user = userService.findById(id);
+
+        return ResponseEntity.ok(user);
     }
 }
--- src/main/java/com/hakkinenT/dspost/services/UserService.java
+++ src/main/java/com/hakkinenT/dspost/services/UserService.java
@@ -3,10 +3,12 @@ package com.hakkinenT.dspost.services;
 import com.hakkinenT.dspost.models.dto.UserDTO;
 import com.hakkinenT.dspost.models.entities.User;
 import com.hakkinenT.dspost.repositories.UserRepository;
+import com.hakkinenT.dspost.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
     public List<UserDTO> findAll(){
         List<User> users = userRepository.findAll();
         return users.stream().map(UserDTO::new).toList();
+    }
+
+    public UserDTO findById(String id){
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado!"));
+        return new UserDTO(user);
     }
 }

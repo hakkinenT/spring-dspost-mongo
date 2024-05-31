0481bf82f5b2db9b92ad1ae9f3f1bae0a2c55deb
Tawanna <thol_16@hotmail.com>
2024-05-30 21:20:47 -0300

    Project created

---
+++ src/main/java/com/hakkinenT/dspost/DspostApplication.java
@@ -0,0 +1,13 @@
+package com.hakkinenT.dspost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DspostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hakkinenT/dspost/DspostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakkinenT.dspost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
